d3a22e12fddaa99c90b6516f89b640ff8df917b0
erri120 <erri120@fossmailer.de>
2026-02-23 15:23:28 +0100

    Formatting

--- InstallAssets.js
+++ InstallAssets.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const Promise = require('bluebird');
-const fs = require('fs-extra');
-const path = require('path');
-const { glob } = require('glob');
-const exec = require('child_process').exec;
+const Promise = require("bluebird");
+const fs = require("fs-extra");
+const path = require("path");
+const { glob } = require("glob");
+const exec = require("child_process").exec;
 
-const data = require('./InstallAssets.json');
+const data = require("./InstallAssets.json");
 
 const globOptions = { matchBase: true, globstar: true };
 
@@ -26,26 +26,26 @@ for (let spawn of data.spawn) {
     continue;
   }
 
-  const cmdline = spawn.executable + ' ' + spawn.arguments.join(' ');
-  const child = exec(spawn.executable + ' ' + spawn.arguments.join(' '), {
+  const cmdline = spawn.executable + " " + spawn.arguments.join(" ");
+  const child = exec(spawn.executable + " " + spawn.arguments.join(" "), {
     stdio: [0, 1, 2],
-    env: Object.assign({}, process.env, spawn.env)
+    env: Object.assign({}, process.env, spawn.env),
   });
-  console.log('spawned', cmdline);
-  child.stdout.on('data', (output) => {
+  console.log("spawned", cmdline);
+  child.stdout.on("data", (output) => {
     console.log(spawn.executable, output);
   });
-  child.stderr.on('data', (output) => {
-    console.log('Error:', spawn.executable, output);
+  child.stderr.on("data", (output) => {
+    console.log("Error:", spawn.executable, output);
   });
-  child.on('close', (code) => {
+  child.on("close", (code) => {
     if (code !== 0) {
       status = 1;
     }
-    console.log('finished', spawn.executable, code);
+    console.log("finished", spawn.executable, code);
   });
   childProcesses.push(spawn.executable);
-  child.on('exit', () => {
+  child.on("exit", () => {
     childProcesses = childProcesses.filter((proc) => proc !== spawn.executable);
   });
 }
@@ -54,12 +54,12 @@ function waitForProcesses() {
   let resolve;
 
   const cb = () => {
-    if ((childProcesses.length > 0) || (copies !== 0)) {
+    if (childProcesses.length > 0 || copies !== 0) {
       setTimeout(cb, 100);
     } else {
       resolve();
     }
-  }
+  };
 
   return new Promise((resolveIn, reject) => {
     resolve = resolveIn;
@@ -68,34 +68,38 @@ function waitForProcesses() {
 }
 
 // copy files
-Promise.mapSeries(data.copy, file => {
+Promise.mapSeries(data.copy, (file) => {
   if (file.target.indexOf(tgt) === -1) {
     return;
   }
 
   return glob(file.srcPath, globOptions)
-    .then(files => {
-      copies = copies === -1 ? files.length : copies += files.length;
+    .then((files) => {
+      copies = copies === -1 ? files.length : (copies += files.length);
       return files;
     })
-    .then(files => Promise.map(files, (globResult) => {
-      let globTarget = path.join(...globResult.split(/[\/\\]/).slice(file.skipPaths));
-      if (file.rename) {
-        globTarget = path.join(path.dirname(globTarget), file.rename);
-      }
-      const targetFile = path.join(tgt, file.outPath, globTarget);
+    .then((files) =>
+      Promise.map(files, (globResult) => {
+        let globTarget = path.join(
+          ...globResult.split(/[\/\\]/).slice(file.skipPaths),
+        );
+        if (file.rename) {
+          globTarget = path.join(path.dirname(globTarget), file.rename);
+        }
+        const targetFile = path.join(tgt, file.outPath, globTarget);
 
-      return fs.ensureDir(path.dirname(targetFile))
-        .then(() => fs.copy(globResult, targetFile))
-        .then(() => {
-          console.log('copied', globResult, targetFile);
-        })
-        .catch((copyErr) => {
-          console.log('failed to copy', globResult, targetFile, copyErr);
-        })
-        .finally(() => {
-          --copies;
-        });
-    }));
-})
-  .then(() => waitForProcesses());
+        return fs
+          .ensureDir(path.dirname(targetFile))
+          .then(() => fs.copy(globResult, targetFile))
+          .then(() => {
+            console.log("copied", globResult, targetFile);
+          })
+          .catch((copyErr) => {
+            console.log("failed to copy", globResult, targetFile, copyErr);
+          })
+          .finally(() => {
+            --copies;
+          });
+      }),
+    );
+}).then(() => waitForProcesses());
--- createMD5List.js
+++ createMD5List.js
@@ -1,14 +1,14 @@
-const crypto = require('crypto');
-const fs = require('fs').promises;
-const path = require('path');
+const crypto = require("crypto");
+const fs = require("fs").promises;
+const path = require("path");
 
 async function walk(base, rel) {
   let files = [];
   const dirs = [];
 
   try {
-    await Promise.all((await fs.readdir(path.join(base, rel)))
-      .map(async name => {
+    await Promise.all(
+      (await fs.readdir(path.join(base, rel))).map(async (name) => {
         const stats = await fs.stat(path.join(base, rel, name));
         const rPath = path.join(rel, name);
         if (stats.isDirectory()) {
@@ -16,14 +16,17 @@ async function walk(base, rel) {
         } else {
           files.push(rPath);
         }
-      }));
+      }),
+    );
 
-    await Promise.all(await dirs.map(async dir => {
-      const rec = await walk(base, dir);
-      files = [].concat(files, rec);
-    }));
+    await Promise.all(
+      await dirs.map(async (dir) => {
+        const rec = await walk(base, dir);
+        files = [].concat(files, rec);
+      }),
+    );
   } catch (err) {
-    console.error('Failed to walk', base, err);
+    console.error("Failed to walk", base, err);
   }
 
   return files;
@@ -40,32 +43,34 @@ async function processBatch(files, batchSize, processor) {
   return results;
 }
 
-exports.default = async function(context) {
+exports.default = async function (context) {
+  console.log("createMD5List.js");
 
-  console.log('createMD5List.js');
+  const assetsPath = path.join(
+    context.appOutDir,
+    "resources",
+    "app.asar.unpacked",
+    "assets",
+  );
 
-  const assetsPath = path.join(context.appOutDir, 'resources', 'app.asar.unpacked', 'assets');
-
-  const allFiles = await walk(context.appOutDir, '');
+  const allFiles = await walk(context.appOutDir, "");
   console.log(`Processing ${allFiles.length} files in batches of 100...`);
 
   const hashes = await processBatch(allFiles, 100, async (relPath) => {
-    const hash = crypto.createHash('md5');
+    const hash = crypto.createHash("md5");
     const fileData = await fs.readFile(path.join(context.appOutDir, relPath));
-    const buf = hash
-      .update(fileData)
-      .digest();
-    console.log(`${relPath}:${buf.toString('hex')}`);
-    return `${relPath}:${buf.toString('hex')}`;
+    const buf = hash.update(fileData).digest();
+    console.log(`${relPath}:${buf.toString("hex")}`);
+    return `${relPath}:${buf.toString("hex")}`;
   });
 
   try {
-    await fs.writeFile(path.join(assetsPath, 'md5sums.csv'), hashes.join('\n'));
+    await fs.writeFile(path.join(assetsPath, "md5sums.csv"), hashes.join("\n"));
   } catch (err) {
     console.error(`Failed to write: ${err.message}`);
   }
-  
-  console.log(`Successfully wrote ${path.join(assetsPath, 'md5sums.csv')}`);
 
-  return [path.join(assetsPath, 'md5sums.csv')];
-}
+  console.log(`Successfully wrote ${path.join(assetsPath, "md5sums.csv")}`);
+
+  return [path.join(assetsPath, "md5sums.csv")];
+};
